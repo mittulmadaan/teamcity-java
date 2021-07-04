891072b4288ca3b7b4ba3f769d6138236ef80a0e
Mittul Madaan <mittulmadaan@gmail.com>
2021-07-04 14:09:31 +0100

    Update App.java

--- src/main/java/com/infojolt/App.java
+++ src/main/java/com/infojolt/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello NAGP team");
+        System.out.println( "Hello NAGP team, welcome");
     }
 }
